fdd25fe8b126947b84639b5b0ddf08132a049d0e
garrettroell <garrettroell@gmail.com>
2023-07-19 17:33:24 -0400

    updated the molecular inventory section text

--- src/HomePage/InventorySection/InventorySection.jsx
+++ src/HomePage/InventorySection/InventorySection.jsx
@@ -7,16 +7,16 @@ const InventorySection = () => {
         <Heading textAlign="center">Molecular Inventory</Heading>
         <Spacer minH="10px" />
         <Text textAlign="justify" maxW="800px" opacity="0.8">
-          As we strive to develop machine learning algorithms for bioprocess
-          fermentations, we often encounter data involving numerous substrates
-          and products. Instead of employing the individual names of these
-          molecules for classification, we find it more practical to utilize
-          their inherent properties for model training. These properties,
-          independent of specific fermentation details, can be stored in a
-          standalone database apart from the fermentation results database. We
-          refer to this database as the Molecular Inventory, which is now
-          available for download to expedite your machine learning model
-          development efforts.
+          Machine learning models used for industrial biological processes must
+          work with various types of fermentations, utilizing many types of
+          carbon sources to create a wide range of products. Training these
+          models with the properties of molecules rather than their names allows
+          predictions to be made with carbon sources and products not included
+          in the training data. These molecular characteristics do not vary
+          across fermentations, so they can be kept in a separate database. This
+          database, which we call the Molecular Inventory, can now be downloaded
+          for free by researchers working on developing machine learning
+          approaches for biological processes.
         </Text>
         <Spacer minH="4px" />
         <a href="https://garrettroell.com/molecular-inventory">
