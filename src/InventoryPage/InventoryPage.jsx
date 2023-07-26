22a1830b166eba5c9b7369c6ac09086bd3369d9b
garrettroell <garrettroell@gmail.com>
2023-07-26 12:35:48 -0400

    updated dark mode styles/formatting of InventoryPage

--- src/InventoryPage/InventoryPage.jsx
+++ src/InventoryPage/InventoryPage.jsx
@@ -1,26 +1,36 @@
 import NavBar from "../NavBar/NavBar";
 import LoadingDots from "../loadingDots/LoadingDots";
-import { Box, 
-  Heading, 
-  Spacer, 
-  Button, 
-  HStack, 
-  useDisclosure, 
-  Modal, 
-  ModalOverlay, 
-  ModalContent, 
-  ModalHeader, 
-  ModalCloseButton, 
+import {
+  Box,
+  Heading,
+  Spacer,
+  Button,
+  HStack,
+  useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
   ModalBody,
-  Text} from "@chakra-ui/react";
+  Text,
+  useColorModeValue,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+} from "@chakra-ui/react";
 import { getInventory, downloadInventoryCSV } from "../Helpers/databaseHelpers";
 import { useQuery } from "@tanstack/react-query";
-import { Table, Thead, Tbody, Tr, Th, Td } from '@chakra-ui/react';
 import ReportIssueFormInventory from "./ReportIssueForm";
 import { Link } from "react-router-dom";
 import { ArrowBackIcon } from "@chakra-ui/icons";
 
 const InventoryPage = () => {
+  let headingColor = useColorModeValue("green.600", "green.100");
+
   // get either cached or new molecular inventory data
   const { isLoading, data } = useQuery(["inventory"], ({ queryKey }) =>
     getInventory()
@@ -28,7 +38,6 @@ const InventoryPage = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   // handle loading state
   if (isLoading) {
-    
     return (
       <>
         <NavBar />
@@ -52,9 +61,8 @@ const InventoryPage = () => {
         {/* outer container */}
         <Box maxW="1500px" mt="35px" mx="auto">
           {/* inner container */}
-          <Box maxW="900px" px="20px">
-
-            <Box marginBottom="20px">
+          <Box maxW="900px" pl="20px">
+            <Box mb="20px">
               <Link to={"/"}>
                 <HStack spacing="4px">
                   <ArrowBackIcon opacity="0.8" />
@@ -65,23 +73,27 @@ const InventoryPage = () => {
               </Link>
             </Box>
 
-            
-
-            <HStack w="full" justifyContent="space-between" mt="5px"mb="20px">
-              <Heading
-                fontSize="20px"
-                mt="5px"
-                mb="20px"
-                color="green.600">
-                  Current Inventory
+            <HStack w="full" justifyContent="space-between" mt="5px" mb="20px">
+              <Heading fontSize="20px" mt="5px" mb="20px" color={headingColor}>
+                Molecular Inventory
               </Heading>
 
               <HStack>
-                <Button bg="green.100"color="gray.800" size="sm" onClick={() => downloadInventoryCSV(data)}>
+                <Button
+                  bg="green.100"
+                  color="gray.800"
+                  size="sm"
+                  onClick={() => downloadInventoryCSV(data)}
+                >
                   Download Data
                 </Button>
-                <Button color="gray.800" size="sm" onClick={onOpen}>
-                  Report an Issue
+                <Button
+                  variant="outline"
+                  size="sm"
+                  opacity="0.8"
+                  onClick={onOpen}
+                >
+                  Report an issue
                 </Button>
               </HStack>
               <Modal
@@ -100,54 +112,52 @@ const InventoryPage = () => {
                 </ModalContent>
               </Modal>
             </HStack>
-          
           </Box>
           <Box maxW="900px" px="20px" overflowX="auto">
-          <Table variant='striped'>
-            <Thead>
-              <Tr>
-                <Th>Product</Th>
-                <Th>Number of results in Yarrowia Database</Th>
-                <Th>Estimated M.W. of products</Th>
-                <Th>Product Delta G of formation</Th>
-                <Th>Central carbon precursor</Th>
-                <Th>Central carbon precursor Delta G of formation</Th>
-                <Th>Pathway enzymatic steps</Th>
-                <Th># precursors required</Th>
-                <Th>ATP cost</Th>
-                <Th>Cofactor cost</Th>
-                <Th># carbons in product</Th>
-                <Th># hydrogens in product</Th>
-                <Th># oxygens in product</Th>
-                <Th># nitrogens in product</Th>
-                <Th>Theoretical Yield(mol Product/mol Glucose)</Th>
-                <Th>solubility g/L(25C)</Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              {data.map((item, index) => (
-                <Tr key={index}>
-                  <Td fontSize="sm">{item.Product}</Td>
-                  <Td fontSize="sm">{item.num_yarrowia_results}</Td>
-                  <Td fontSize="sm">{item.molecularWeight}</Td>
-                  <Td fontSize="sm">{item.deltaGFormation}</Td>
-                  <Td fontSize="sm">{item.precursors}</Td>
-                  <Td fontSize="sm">{item.precursorDeltaGFormation}</Td>
-                  <Td fontSize="sm">{item.numberEnzymaticSteps}</Td>
-                  <Td fontSize="sm">{item.numberPrecursorsRequired}</Td>
-                  <Td fontSize="sm">{item.ATPCost}</Td>
-                  <Td fontSize="sm">{item.CofactorCost}</Td>
-                  <Td fontSize="sm">{item.NumCarbons}</Td>
-                  <Td fontSize="sm">{item.numHydrogens}</Td>
-                  <Td fontSize="sm">{item.numOxygens}</Td>
-                  <Td fontSize="sm">{item.numNitrogens}</Td>
-                  <Td fontSize="sm">{item.TheoreticalYieldPerMolGlucose}</Td>
-                  <Td fontSize="sm">{item.solubilityGPerL}</Td>
+            <Table variant="striped">
+              <Thead>
+                <Tr>
+                  <Th>Product</Th>
+                  <Th>Number of results in Yarrowia Database</Th>
+                  <Th>Estimated M.W. of products</Th>
+                  <Th>Product Delta G of formation</Th>
+                  <Th>Central carbon precursor</Th>
+                  <Th>Central carbon precursor Delta G of formation</Th>
+                  <Th>Pathway enzymatic steps</Th>
+                  <Th># precursors required</Th>
+                  <Th>ATP cost</Th>
+                  <Th>Cofactor cost</Th>
+                  <Th># carbons in product</Th>
+                  <Th># hydrogens in product</Th>
+                  <Th># oxygens in product</Th>
+                  <Th># nitrogens in product</Th>
+                  <Th>Theoretical Yield(mol Product/mol Glucose)</Th>
+                  <Th>solubility g/L(25C)</Th>
                 </Tr>
-              ))}
-            </Tbody>
-          </Table>
-
+              </Thead>
+              <Tbody>
+                {data.map((item, index) => (
+                  <Tr key={index}>
+                    <Td fontSize="sm">{item.Product}</Td>
+                    <Td fontSize="sm">{item.num_yarrowia_results}</Td>
+                    <Td fontSize="sm">{item.molecularWeight}</Td>
+                    <Td fontSize="sm">{item.deltaGFormation}</Td>
+                    <Td fontSize="sm">{item.precursors}</Td>
+                    <Td fontSize="sm">{item.precursorDeltaGFormation}</Td>
+                    <Td fontSize="sm">{item.numberEnzymaticSteps}</Td>
+                    <Td fontSize="sm">{item.numberPrecursorsRequired}</Td>
+                    <Td fontSize="sm">{item.ATPCost}</Td>
+                    <Td fontSize="sm">{item.CofactorCost}</Td>
+                    <Td fontSize="sm">{item.NumCarbons}</Td>
+                    <Td fontSize="sm">{item.numHydrogens}</Td>
+                    <Td fontSize="sm">{item.numOxygens}</Td>
+                    <Td fontSize="sm">{item.numNitrogens}</Td>
+                    <Td fontSize="sm">{item.TheoreticalYieldPerMolGlucose}</Td>
+                    <Td fontSize="sm">{item.solubilityGPerL}</Td>
+                  </Tr>
+                ))}
+              </Tbody>
+            </Table>
           </Box>
         </Box>
       </>
